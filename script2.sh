db1c0239cf259f09573f1d367e42a358c2949cee
herkesanlamaz46-cmyk <herkesanlamaz46@gmail.com>
2026-02-14 22:15:56 +0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -12,36 +12,26 @@ elif [ -f vendor_ramdisk_recovery.cpio ]; then
     ramdisk="vendor_ramdisk_recovery.cpio"
 fi
 
+# Ramdisk çıkar
 ../magiskboot cpio "$ramdisk" extract
 
-# Reverse fastbootd ENG mode check
-../magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094
-../magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
-../magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
-../magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
+# === SADECE GEREKLİ 3 PATCH ===
+# Fastboot kontrol
 ../magiskboot hexpatch system/bin/recovery 080109aae80000b4 080109aae80000b5
-../magiskboot hexpatch system/bin/recovery 20f0a6ef38b1681c 20f0a6ef38b9681c
-../magiskboot hexpatch system/bin/recovery 23f03aed38b1681c 23f03aed38b9681c
-../magiskboot hexpatch system/bin/recovery 20f09eef38b1681c 20f09eef38b9681c
-../magiskboot hexpatch system/bin/recovery 26f0ceec30b1681c 26f0ceec30b9681c
-../magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
-../magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
-../magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
-../magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
-../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
-../magiskboot hexpatch system/bin/recovery 2001597ae0000054 2001597ae1000054
-../magiskboot hexpatch system/bin/recovery 2001597ac0000054 2001597ac1000054
-../magiskboot hexpatch system/bin/recovery 9ef0fcec28b1701c 9ef0fced28b1701c
-../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
-../magiskboot hexpatch system/bin/recovery 24f0f2ea30b1681c 24f0f2ea30b9681c
-../magiskboot hexpatch system/bin/recovery e0031f2a8e000014 200080528e000014
-../magiskboot hexpatch system/bin/recovery 41010054a0020012f44f48a9 4101005420008052f44f48a9
 
+# ENG mode kontrol
+../magiskboot hexpatch system/bin/recovery 2001597ae0000054 2001597ae1000054
+
+# Recovery binary bypass
+../magiskboot hexpatch system/bin/recovery e0031f2a8e000014 200080528e000014
+
+# Binary geri ekle
 ../magiskboot cpio "$ramdisk" 'add 0755 system/bin/recovery system/bin/recovery'
 
+# Repack
 ../magiskboot repack ../r.img new-boot.img
 
 cd ..
 cp unpack/new-boot.img recovery-patched.img
 
-echo "Done: recovery-patched.img oluşturuldu"
+echo "Done: recovery-patched.img hazır"
