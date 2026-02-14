83bb5b9dce46bcc65e21bc1af5412c96b217cf8d
herkesanlamaz46-cmyk <herkesanlamaz46@gmail.com>
2026-02-14 14:51:15 +0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir unpack
-cd unpack
+cd unpack || exit 1
+
 ../magiskboot unpack ../r.img
 
 ramdisk="ramdisk.cpio"
@@ -12,8 +13,9 @@ elif [ -f vendor_ramdisk_recovery.cpio ]; then
 fi
 
 ../magiskboot cpio "$ramdisk" extract
+
 # Reverse fastbootd ENG mode check
-../magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
+../magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094
 ../magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
 ../magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
 ../magiskboot hexpatch system/bin/recovery 50c0009420010034 50c0009420010035
@@ -27,13 +29,19 @@ fi
 ../magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
 ../magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
 ../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
-../magiskboot hexpatch system/bin/recovery 2001597ae0000054 2001597ae1000054 # ccmp w9, w25, #0, eq ; b.e #0x20 ===> b.ne #0x20
-../magiskboot hexpatch system/bin/recovery 2001597ac0000054 2001597ac1000054 # ccmp w9, w25, #0, eq ; b.e #0x1c ===> b.ne #0x1c
+../magiskboot hexpatch system/bin/recovery 2001597ae0000054 2001597ae1000054
+../magiskboot hexpatch system/bin/recovery 2001597ac0000054 2001597ac1000054
 ../magiskboot hexpatch system/bin/recovery 9ef0fcec28b1701c 9ef0fced28b1701c
 ../magiskboot hexpatch system/bin/recovery 9ef00ced28b1701c 9ef00ced28b9701c
 ../magiskboot hexpatch system/bin/recovery 24f0f2ea30b1681c 24f0f2ea30b9681c
 ../magiskboot hexpatch system/bin/recovery e0031f2a8e000014 200080528e000014
 ../magiskboot hexpatch system/bin/recovery 41010054a0020012f44f48a9 4101005420008052f44f48a9
+
 ../magiskboot cpio "$ramdisk" 'add 0755 system/bin/recovery system/bin/recovery'
+
 ../magiskboot repack ../r.img new-boot.img
-cp new-boot.img ../r-patched.img
+
+cd ..
+cp unpack/new-boot.img recovery-patched.img
+
+echo "Done: recovery-patched.img oluşturuldu"
